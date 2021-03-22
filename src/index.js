f3aa8a89101f1d060f1022ef9c4ffb7e9180181d
Danny <danny.costelloe@gmail.com>
2021-03-22 14:20:14 -0400

    commit tree 25536eacb2f1aa9705fc5b7b8d245f64fc171d0c

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+
+
+fetch("https://dog.ceo/api/breeds/image/random/4" )
+    .then(res => res.json)
+    .then(function(dogRes)){
+        
+        let blankli = document.createElement("li")
+        let blankImg = document.createElement("img")
+
+            blankImg.src= dogRes.message
+            blankImg.alt = "dog"
+
+        blankli.append(blankImg)
+        dogsUl.append(blankli)
+    }
